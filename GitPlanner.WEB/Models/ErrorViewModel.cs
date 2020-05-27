bd3c3406bf69c14105de53b9b90fa1aeb1904f5e
Jean Ruffato <jean.ruffato@gmail.com>
2020-05-26 21:28:03 -0300

    Commit Inicial

---
+++ GitPlanner.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitPlanner.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
